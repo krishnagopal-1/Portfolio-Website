94de9202bdc9dd0ea68441426f6e474231136a3a
Krishna Gopal <krishnagopal0508@gmail.com>
2025-08-01 16:15:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const menu = document.querySelector("#menu");
+const menu = document.getElementById("menu");
+const btn = document.querySelector(".links");
 
-const nav = document.querySelector(".links");
+menu.addEventListener("click", function () {
+    btn.classList.toggle('active');
 
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    nav.classList.toggle('active');
-}
+    if (menu.classList.contains('ri-menu-5-line')) {
+
+        menu.classList.remove('ri-menu-5-line');
+        menu.classList.add('ri-close-line');
+
+    } else {
+
+        menu.classList.remove('ri-close-line');
+        menu.classList.add('ri-menu-5-line');
+
+    }
+});
